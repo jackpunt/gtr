771fdf847b2cb71435442d9d2b0cc978c8fc1247
jackpunt <jpeckj@gmail.com>
2025-07-27 10:50:41 -0700

    works with dpi:1

--- src/app/gtr-card.ts
+++ src/app/gtr-card.ts
@@ -1,6 +1,5 @@
-import { ImageGrid, type CountClaz, type GridSpec, type Paintable } from "@thegraid/easeljs-lib";
+import { type CountClaz, type GridSpec, type Paintable } from "@thegraid/easeljs-lib";
 import { AliasLoader, Tile } from "@thegraid/hexlib";
-import { TileExporter } from "./tile-exporter";
 import { C } from "@thegraid/common-lib";
 import { CardShape } from "./card-shape";
 
@@ -8,11 +7,18 @@ import { CardShape } from "./card-shape";
 export class GtrCard extends Tile {
   // Grey-000:009, Red-010:019, Yellow-014:018, Brown-019:023, Blue-021:030, Purple-031:040,
   // Odd-000:013
-  static names: Record<string, number> = {
-    "Red-010": 3,
-    "Red-011": 3,
+  static names3: Record<string, number> = {
+    // "Odd-013-Back": 2,    // back of Card
+    "Odd-000-Jack": 2,    // back of Card
+    // "Red-014": 3,
+    // "Red-015": 3,
+    // "Red-016": 3,
+    // "Grey-000": 3,
+    "Grey-001": 3,
+    "Grey-002": 2,
   }
-  static names0: Record<string, number> = {
+  static namesAll: Record<string, number> = {
+    "Odd-013-Back": 8,    // back of Card
     "Grey-000": 3,
     "Grey-001": 3,
     "Grey-002": 3,
@@ -88,14 +94,16 @@ export class GtrCard extends Tile {
     "Odd-010-Concrete": -6,
     "Odd-011-Marble": -6,
     "Odd-012-Stone": -6,
-    "Odd-013-Back": -1,    // back of Card
 
-    "Odd-099-order": 5,   // Player Aid
-    "GtrLeaderCard": 1,
+    "Odd-099-order": 2,   // Player Aid
+    "GtrLeaderCard": 2,
     // "Odd-098-Blank": 3,
     // "GtrLegionaryRule": 3,
   };
 
+  static names = GtrCard.names3;
+
+  /** filenames of images to load */
   static get fnames() {
     return  [... Object.keys(GtrCard.names)];
   }
@@ -107,11 +115,19 @@ export class GtrCard extends Tile {
     });
   }
 
-  // 8 x 10 @ 300 dpi; 3.5" x 2.5"
-  static myGrid: GridSpec = {
-    width: 2400, height: 3000, nrow: 4, ncol: 2, cardw: 1050, cardh: 750, double: false,
-    x0: 60 + 1108/2, y0: 50 + 808/2, delx: 1052, dely: 752, bleed: 0, bgColor: 'white',
+  // Note: 1108 = 1050 + 2 * (bleed-1); 808 = 750 + 2 * (bleed-1)
+  // indenting each by 2px to show cut-lines
+  static cardSingle_3_5_home: GridSpec = {
+    width: 8.25*300, height: 10.85*300, nrow: 4, ncol: 2, cardw: 1050-2, cardh: 750-2, double: false,
+    x0: 150 + 1050/2, y0: 100 + 750/2, delx: 1050, dely: 750, bleed: 0, bgColor: 'white',
+  }
+  static cardSingle_3_5_home_dpi: GridSpec = {
+    width: 8.25, height: 10.85, nrow: 4, ncol: 2, cardw: 3.5, cardh: 2.5, double: false,
+    x0: .5 + 3.5/2, y0: .33 + 2.5/2, delx: 3.5, dely: 2.5, bleed: 0, bgColor: 'white', dpi: 300,
   };
+  // 8 x 10 @ 300 dpi; 3.5" x 2.5"
+  // ImageGrid.cardSingle_3_5_home (dpi=1 vs x=3.5 @ 300 dpi)
+  static myGrid: GridSpec = GtrCard.cardSingle_3_5_home;
 
   // 808 x 1108; 750 x 1050
   // gridSpec = ImageGrid.cardSingle_3_5; // cardw: 1050, cardh: 750
@@ -119,7 +135,11 @@ export class GtrCard extends Tile {
   color = C.WHITE;
 
   // shorter side of card; longer is radius*1.4
-  override get radius() { return GtrCard.myGrid?.cardh ?? 750; } // nextRadius
+  override get radius() {
+    const { cardh: wide, dpi } = GtrCard.myGrid;
+    const wdpi = wide ? ((dpi ? wide : wide+2) * (dpi ?? 1)) : 808;
+    return wdpi;
+  } // nextRadius
 
   constructor(Aname: string, gridSpec: GridSpec) {
     super(Aname);
@@ -136,6 +156,7 @@ export class GtrCard extends Tile {
     const h = this.gridSpec.cardh!;
     const bmImage = AliasLoader.loader.getBitmap(this.Aname, this.gridSpec.cardw!); // scaled to fit cardw
     const { x, y, height, width } = this.baseShape.getBounds();
+    // bmImage.image: [808 x 1108]
     if (bmImage) {
       // bmImage.x -= (width - bmImage.image.width)/2;   // center image on card, may lose the 'bleed' from image
       // bmImage.y -= (height - bmImage.image.height)/2; //
--- src/app/tile-exporter.ts
+++ src/app/tile-exporter.ts
@@ -1,4 +1,4 @@
-import { PageSpec, TileExporter as TileExporterLib, type CountClaz, type GridSpec } from "@thegraid/easeljs-lib";
+import { PageSpec, TileExporter as TileExporterLib, type CountClaz } from "@thegraid/easeljs-lib";
 import { GtrCard } from "./gtr-card";
 // import { CubeCard } from "./cube-card";
 
@@ -6,38 +6,17 @@ import { GtrCard } from "./gtr-card";
 
 export class TileExporter extends TileExporterLib {
 
-  // x0 = pixel margin + bleed + width/2
-  static euroPoker: GridSpec = {
-    width: 3600, height: 5400, nrow: 6, ncol: 3, cardw: 1040, cardh: 734, double: false,
-    x0: 158 + 30 + 1040/2, y0: 320 + 30 + 734/2, delx: 1122.5, dely: 803, bleed: 30,
-  }
-
-  /** for home printer 8x10 @ 300dpi = 2400 x 3000 */
-  static euroPoker2: GridSpec = {
-    width: 2400, height: 3000, nrow: 4, ncol: 2, cardw: 1040, cardh: 734, double: false,
-    x0: 60 + 1040/2, y0: 25 + 734/2, delx: 1122.5, dely: 734, bleed: 0, bgColor: 'white',
-  }
-
-  // for home printer 8x10 @ 300 dpi
-  // // 808 x 1108
-  static stdPoker2: GridSpec = {
-    width: 2400, height: 3000, nrow: 4, ncol: 2, cardw: 1108, cardh: 808, double: false,
-    x0: 60 + 1108/2, y0: 50 + 808/2, delx: 1122.5, dely: 810, bleed: 0, bgColor: 'white',
-  }
-
   gridSpec = GtrCard.myGrid;
 
   // invoked by onclick('makePage')
   override makeImagePages() {
-    // [...[count, claz, ...constructorArgs]]
-    const cardSingle_euro_back = [
-    ] as CountClaz[];
-    const cardSingle_euro_base = [
+    // CountClaz[] --> [...[count, claz, ...constructorArgs]]
+    const cardSingle_front = [
       ...GtrCard.allCards(),
     ] as CountClaz[];
 
     const pageSpecs: PageSpec[] = [];
-    this.clazToTemplate(cardSingle_euro_base, this.gridSpec, pageSpecs);
+    this.clazToTemplate(cardSingle_front, this.gridSpec, pageSpecs);
     return pageSpecs;
   }
 
